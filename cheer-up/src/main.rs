a067023ae859d2049bfa103d4770ebecfe0e0dfc
tanque <paoletti.francesco@gmail.com>
2024-03-02 03:18:08 +0100

    feat(cheer-up): bot replies with copyat video note

--- cheer-up/src/main.rs
+++ cheer-up/src/main.rs
@@ -1,5 +1,6 @@
 use log;
-use teloxide::prelude::*;
+use teloxide::{net::Download, prelude::*, types::InputFile};
+use tokio::fs;
 
 #[tokio::main]
 async fn main() {
@@ -10,7 +11,23 @@ async fn main() {
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         log::info!("Throwing dice bot...");
-        bot.send_dice(msg.chat.id).await?;
+        println!("Throwing dice bot...");
+        println!("msg.video_note(): {:?}", msg.video_note());
+        match msg.video_note() {
+            Some(vnote) => {
+                //
+                let vnote_file_id = vnote.file.id.clone();
+                println!("vnote: {:?}", vnote);
+                println!("vnote.id: {:?}", vnote.file.id);
+
+                bot.send_video_note(msg.chat.id, InputFile::file_id(vnote_file_id))
+                    .await?;
+
+                println!("vnote sent to {:?}", msg.chat.id.to_string())
+            }
+            None => println!("msg is not a video note"),
+        }
+        // bot.send_dice(msg.chat.id).await?;
         Ok(())
     })
     .await;
