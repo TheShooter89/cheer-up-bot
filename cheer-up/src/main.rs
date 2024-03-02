a56426368c2d992a07f37d19edc05a546557abb7
tanque <paoletti.francesco@gmail.com>
2024-03-02 01:38:00 +0100

    feat(cheer-up): add placeholder dice throwing bot

--- cheer-up/src/main.rs
+++ cheer-up/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use log;
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    log::info!("Starting throw dice bot...");
+    dotenvy::dotenv().ok();
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        log::info!("Throwing dice bot...");
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
