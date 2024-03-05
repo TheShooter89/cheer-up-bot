cb7ed9f3428efefa0075b803111c83628e462b96
tanque <paoletti.francesco@gmail.com>
2024-03-05 18:55:54 +0100

    (#2) feat(cheer-up-manager): add all bot commands rendering mock replies

--- cheer-up-manager/src/main.rs
+++ cheer-up-manager/src/main.rs
@@ -88,6 +88,30 @@ async fn start_command(bot: Bot, msg: Message) -> ResponseResult<()> {
     Ok(())
 }
 
+async fn list_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::StartPage;
+    println!("{}", template.render());
+    Ok(())
+}
+
+async fn erase_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::StartPage;
+    println!("{}", template.render());
+    Ok(())
+}
+
+async fn help_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::StartPage;
+    println!("{}", template.render());
+    Ok(())
+}
+
+async fn credits_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::StartPage;
+    println!("{}", template.render());
+    Ok(())
+}
+
 async fn download_vnote(bot: &Bot, file_id: &str, chat_id: ChatId) -> Result<(), RequestError> {
     let file = bot.get_file(file_id).await?;
 
