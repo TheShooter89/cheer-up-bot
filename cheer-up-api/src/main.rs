e3dfd21eb77febca7bb6e31430bbbc49b0c847b2
tanque <paoletti.francesco@gmail.com>
2024-03-14 21:28:03 +0100

    (#13) feat(cheer-up-api): add tracing logging

--- cheer-up-api/src/main.rs
+++ cheer-up-api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+extern crate pretty_env_logger;
+
+use anyhow::Ok;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[macro_use]
+extern crate log;
+
+fn main() -> Result<(), anyhow::Error> {
+    pretty_env_logger::init();
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(
+            |_| "axum_login=debug,tower_sessions=debug,sqlx=warn,tower_http=debug".into(),
+        )))
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
+
+    info!("Hello, world!");
+    Ok(())
 }
