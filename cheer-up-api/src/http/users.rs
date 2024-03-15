de6e36391714abc55be4a981b76366e30831d244
tanque <paoletti.francesco@gmail.com>
2024-03-15 18:14:32 +0100

    (#15) feat(cheer-up-api/http): add users module and types

---
+++ cheer-up-api/src/http/users.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    id: u64,
+    telegram_id: u64,
+    username: String,
+    first_name: String,
+    last_name: Option<String>,
+}
+
+#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(default)]
+struct UpdateUser {
+    id: Option<u64>,
+    telegram_id: Option<u64>,
+    username: Option<String>,
+    first_name: Option<String>,
+    last_name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewUser {
+    telegram_id: u64,
+    username: String,
+    first_name: String,
+    last_name: Option<String>,
+}
