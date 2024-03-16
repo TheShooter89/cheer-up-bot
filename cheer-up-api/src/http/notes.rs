3646e1cdef5336bd3c3f2a4e495f98d2660e90e0
tanque <paoletti.francesco@gmail.com>
2024-03-17 00:22:06 +0100

    (#16) feat(cheer-up-api/notes): fetch one note from database

--- cheer-up-api/src/http/notes.rs
+++ cheer-up-api/src/http/notes.rs
@@ -9,24 +9,26 @@ use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 use crate::http::error::Error;
 use crate::http::http::Result;
 
+use super::users;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Note {
-    id: u64,
-    user_id: u64,
-    file_name: String,
+    pub id: i64,
+    pub user_id: i64,
+    pub file_name: String,
 }
 
 #[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(default)]
 struct UpdateNote {
-    id: Option<u64>,
-    user_id: Option<u64>,
+    id: Option<i64>,
+    user_id: Option<i64>,
     file_name: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct NewNote {
-    user_id: u64,
+    user_id: i64,
     file_name: String,
 }
 
@@ -42,11 +44,24 @@ pub fn router(pool: SqlitePool) -> Router<()> {
 }
 
 pub async fn get_user(State(pool): State<SqlitePool>) -> Result<Json<NoteBody<Note>>> {
-    Ok(Json(NoteBody {
-        note: Note {
-            id: 9,
-            user_id: 28,
-            file_name: "harlock".to_string(),
-        },
-    }))
+    let users = sqlx::query_as!(
+        Note,
+        r#"
+SELECT id, user_id, file_name
+FROM notes
+ORDER BY id
+    "#,
+    )
+    .fetch_one(&pool)
+    .await?;
+
+    Ok(Json(NoteBody { note: users }))
+
+    // Ok(Json(NoteBody {
+    //     note: Note {
+    //         id: 9,
+    //         user_id: 28,
+    //         file_name: "harlock".to_string(),
+    //     },
+    // }))
 }
