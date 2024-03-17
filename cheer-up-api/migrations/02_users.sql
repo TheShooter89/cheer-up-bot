3d88ce267144cecde98b110c45605dfddf324d7c
tanque <paoletti.francesco@gmail.com>
2024-03-17 11:24:27 +0100

    (#15) feat(cheer-up-api/users): replace random user data qith mock data

--- cheer-up-api/migrations/02_users.sql
+++ cheer-up-api/migrations/02_users.sql
@@ -1,5 +1,5 @@
 INSERT OR IGNORE INTO users (telegram_id, username, first_name, last_name)
-VALUES (1234567, 'tanqueshen', 'Francesco', 'Paoletti');
+VALUES (1234567, 'sirwinston', 'winston', 'churchill');
 
 INSERT OR IGNORE INTO users (telegram_id, username, first_name, last_name)
-VALUES (8234541, 'theshooter', 'Tanque', 'Shen');
+VALUES (8234541, 'che', 'ernesto', 'guevara');
