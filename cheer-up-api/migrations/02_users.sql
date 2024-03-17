5c2645f164360026a0bd79f180b99f0feb57b47c
tanque <paoletti.francesco@gmail.com>
2024-03-17 11:20:50 +0100

    (#16) feat(cheer-up-api/users): add new test records to db

--- cheer-up-api/migrations/02_users.sql
+++ cheer-up-api/migrations/02_users.sql
@@ -1,2 +1,5 @@
 INSERT OR IGNORE INTO users (telegram_id, username, first_name, last_name)
 VALUES (1234567, 'tanqueshen', 'Francesco', 'Paoletti');
+
+INSERT OR IGNORE INTO users (telegram_id, username, first_name, last_name)
+VALUES (8234541, 'theshooter', 'Tanque', 'Shen');
