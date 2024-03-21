5ab98f6b054f582c8e0f85fe2a84fabd917118b3
tanque <paoletti.francesco@gmail.com>
2024-03-21 12:15:19 +0100

    (#35) feat(cheer-up): add internationalization using i18n crate

--- cheer-up/src/main.rs
+++ cheer-up/src/main.rs
@@ -1,5 +1,10 @@
 use std::io::Error;
 
+// Load I18n macro, for allow you use `t!` macro in anywhere.
+#[macro_use]
+extern crate rust_i18n;
+rust_i18n::i18n!("locales", fallback = "en");
+
 use log;
 use teloxide::{
     net::Download,
