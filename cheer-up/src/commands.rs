7498596d0788658f1c68c70bfaeff2d91bd08f7a
tanque <paoletti.francesco@gmail.com>
2024-03-21 01:25:23 +0100

    (#33) refactor(cheer-up): clone cheer-up-manager bot code as boilerplate

---
+++ cheer-up/src/commands.rs
@@ -0,0 +1,147 @@
+use std::io::Error;
+
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude::*,
+    types::{InputFile, KeyboardButton, KeyboardMarkup, ParseMode},
+    utils::command::BotCommands,
+};
+use tokio::fs;
+
+use crate::{
+    templates::Templates,
+    utils::get_user_folder_path,
+    videonotes::{delete_all_user_vnotes, get_vnote_list_from_db},
+};
+
+#[derive(Debug, BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+pub enum Command {
+    #[command(description = "CheerUp Bot starting page")]
+    Start,
+    #[command(description = "List all uploaded video notes")]
+    List,
+    #[command(description = "Erase all video notes")]
+    Erase,
+    #[command(description = "Confirm to erase all video notes")]
+    EraseAll,
+    #[command(description = "Show help and available commands")]
+    Help,
+    #[command(description = "Show credits and code repo links")]
+    Credits,
+}
+
+impl Command {
+    pub fn parse_str(cmd: &str) -> Option<Command> {
+        match cmd {
+            "/start" => Some(Command::Start),
+            "/list" => Some(Command::List),
+            "/erase" => Some(Command::Erase),
+            "/help" => Some(Command::Help),
+            "/credits" => Some(Command::Credits),
+            "/eraseall CONFIRM_ERASE" => Some(Command::EraseAll),
+            _ => None,
+        }
+    }
+}
+
+pub async fn handle_commands(bot: Bot, cmd: Command, msg: Message) -> ResponseResult<()> {
+    match cmd {
+        Command::Start => start_command(bot, msg).await?,
+        Command::List => list_command(bot, msg).await?,
+        Command::Erase => erase_command(bot, msg).await?,
+        Command::EraseAll => erase_confirmation_command(bot, msg).await?,
+        Command::Help => help_command(bot, msg).await?,
+        Command::Credits => credits_command(bot, msg).await?,
+    }
+
+    Ok(())
+}
+
+pub async fn start_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let username = match msg.chat.username() {
+        Some(username) => username,
+        None => "Unknown User",
+    };
+
+    let template = Templates::StartPage(username.to_string());
+
+    bot.send_message(msg.chat.id, template.render())
+        .parse_mode(ParseMode::Html)
+        .await?;
+    Ok(())
+}
+
+async fn list_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let vnote_list = get_vnote_list_from_db(&msg.chat).await?;
+    println!("vnote_list is: {:?}", vnote_list);
+
+    let user_folder = get_user_folder_path(&msg.chat);
+
+    for vnote in &vnote_list {
+        let file_path = format!("{}/{}", user_folder, vnote.file_name);
+        bot.send_video_note(msg.chat.id, InputFile::file(file_path))
+            .await?;
+    }
+
+    bot.send_message(
+        msg.chat.id,
+        format!(r"You uploaded <b>{}</b> video notes", vnote_list.len()),
+    )
+    .parse_mode(ParseMode::Html)
+    .await?;
+
+    Ok(())
+}
+
+async fn erase_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::EraseConfirmationPage;
+
+    bot.send_message(msg.chat.id, template.render())
+        .parse_mode(ParseMode::Html)
+        .await?;
+
+    Ok(())
+}
+
+async fn erase_confirmation_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    match delete_all_user_vnotes(&msg.chat).await {
+        Ok(_) => {
+            let template = Templates::EraseConfirmationCompletedPage;
+
+            bot.send_message(msg.chat.id, template.render())
+                .parse_mode(ParseMode::Html)
+                .await?;
+        }
+        Err(_) => {
+            let template = Templates::EraseConfirmationErrorPage;
+
+            bot.send_message(msg.chat.id, template.render())
+                .parse_mode(ParseMode::Html)
+                .await?;
+        }
+    };
+
+    Ok(())
+}
+
+async fn help_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::HelpPage;
+
+    bot.send_message(msg.chat.id, template.render())
+        .parse_mode(ParseMode::Html)
+        .await?;
+
+    Ok(())
+}
+
+async fn credits_command(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let template = Templates::CreditsPage;
+
+    bot.send_message(msg.chat.id, template.render())
+        .parse_mode(ParseMode::Html)
+        .disable_web_page_preview(true)
+        .await?;
+
+    Ok(())
+}
--- cheer-up/src/main.rs
+++ cheer-up/src/main.rs
@@ -1,58 +1,115 @@
 use std::io::Error;
 
 use log;
-use teloxide::{net::Download, prelude::*, types::InputFile, RequestError};
+use teloxide::{
+    net::Download,
+    payloads::SendMessageSetters,
+    prelude::*,
+    types::{InputFile, ParseMode},
+    RequestError,
+};
 use tokio::fs;
+
+mod templates;
+use templates::Templates;
+
+mod commands;
+use commands::*;
+
+mod messages;
+use messages::*;
+
+mod videonotes;
+use videonotes::*;
+
+mod user;
+use user::*;
+
+mod utils;
+use utils::*;
+
+async fn handle_input(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let message_type = MessageType::from_msg(&msg);
+    println!("Message type you sent: {:?}", message_type);
+
+    let chat_id = msg.chat.id;
+
+    match message_type {
+        MessageType::VideoNote => {
+            println!("received video note");
+            let vnote = msg.video_note().unwrap();
+            let vnote_file_id = vnote.file.id.clone();
+
+            let save_user = save_user_to_db(&msg.chat).await;
+            if save_user.is_err() {
+                println!("an error occurred while saving user to db")
+            }
+            upload_vnote(&bot, vnote, &msg.chat).await?;
+            let save_vnote = save_vnote_to_db(vnote, &msg.chat).await;
+            if save_vnote.is_err() {
+                println!("an error occurred while saving note");
+            }
+
+            match download_vnote(&bot, &vnote_file_id, chat_id).await {
+                Ok(res) => println!("vnote succefully saved: {:?}", res),
+                Err(e) => println!("error while saving video note: {:?}", e),
+            }
+            Ok(())
+        }
+        MessageType::Text => {
+            let command = Command::parse_str(msg.text().unwrap_or("none"));
+
+            println!("Text you sent: {:?}", msg.text().unwrap_or("NONE"));
+
+            if let Some(cmd) = command {
+                println!("Command you sent: {:?}", cmd);
+                handle_commands(bot.clone(), cmd, msg.clone()).await?;
+
+                return Ok(());
+            }
+
+            start_command(bot.clone(), msg.clone()).await?;
+
+            Ok(())
+        }
+        _ => {
+            bot.send_message(
+                chat_id,
+                Templates::UnsupportedInputPage(message_type.name().to_string()).render(),
+            )
+            .parse_mode(ParseMode::Html)
+            .await?;
+
+            Ok(())
+        }
+    }
+}
 
 async fn download_vnote(bot: &Bot, file_id: &str, chat_id: ChatId) -> Result<(), RequestError> {
     let file = bot.get_file(file_id).await?;
 
-    let mut output_file = fs::File::create("_EXTRA/test.mpeg").await?;
+    // let folder_name = chat_id.to_string();
+    let folder_name = "../_common_data/videonotes";
+    let file_name = format!("{}/{}.mpeg", folder_name, file_id);
+
+    let mut output_file = fs::File::create(file_name.clone()).await?;
 
     bot.download_file(&file.path, &mut output_file).await?;
 
-    bot.send_video_note(chat_id, InputFile::file("../_common_data//test.mpeg"))
-        .await;
+    bot.send_video_note(chat_id, InputFile::file(file_name))
+        .await?;
 
     Ok(())
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), RequestError> {
+    pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
     dotenvy::dotenv().ok();
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        log::info!("Throwing dice bot...");
-        println!("Throwing dice bot...");
-        println!("msg.video_note(): {:?}", msg.video_note());
-        match msg.video_note() {
-            Some(vnote) => {
-                //
-                let vnote_file_id = vnote.file.id.clone();
-                println!("vnote: {:?}", vnote);
-                println!("vnote.id: {:?}", vnote.file.id);
-
-                // bot.send_video_note(msg.chat.id, InputFile::file_id(vnote_file_id.clone()))
-                //     .await?;
-
-                println!("vnote sent to {:?}", msg.chat.id.to_string());
-
-                match download_vnote(&bot, &vnote_file_id, msg.chat.id).await {
-                    Ok(res) => println!("video note succefully downloaded, result: {:?}", res),
-                    Err(e) => println!("error while downloading video note, error: {:?}", e),
-                };
-                bot.send_message(msg.chat.id, "Specchio riflesso, come alle elementari LOL")
-                    .await
-                    .unwrap();
-                ()
-            }
-            None => println!("msg is not a video note"),
-        }
-        // bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    teloxide::repl(bot, handle_input).await;
+    Ok(())
 }
